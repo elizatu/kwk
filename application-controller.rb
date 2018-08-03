318ec93e8f1d2da9f00350384c3f02277f674e47
EC2 Default User <ec2-user@ip-172-31-17-129.us-east-2.compute.internal>
2018-08-03 17:39:13 +0000

    first

--- application-controller.rb
+++ application-controller.rb
@@ -7,5 +7,21 @@ class MyApp < Sinatra::Base
  get'/' do
   erb :index
  end
+ 
+get '/index.erb' do
+ erb :index
+end
+ 
+get '/donate.erb' do
+ erb :donate
+end
+
+get '/information.erb' do
+ erb :information
+end
+
+get '/bio.erb' do
+ erb :bio
+end
 
 end
